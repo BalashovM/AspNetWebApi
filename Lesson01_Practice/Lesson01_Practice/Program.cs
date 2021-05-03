31d9f44f41062061716da3e255c53bd2b208ab4b
BalashovM <mi-h-a@mail.ru>
2021-05-03 22:48:43 +0700

    Lesson01 - Домашнее задание начало

---
+++ Lesson01_Practice/Lesson01_Practice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lesson01_Practice
+{
+    class Program
+    {
+        private static readonly HttpClient _client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+
+            var result = await GetPosts();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static async Task<List<Post>> GetPosts()
+        {
+            var response = await _client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return new List<Post>();
+        }
+
+
+    }
+}
